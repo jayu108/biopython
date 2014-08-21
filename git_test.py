7643256bc132bf151a1423522557c8c411166281
park <jayu108@gmail.com>
2014-08-21 10:28:01 -0700

    Update git_test.py

--- git_test.py
+++ git_test.py
@@ -44,38 +44,62 @@ print user.html_url  # 사용자 github url 출력.
 
 ****** 사용자 본인 인증 접속하기 ******
 
-g_inst = PyGithub.BlockingBuilder().Login("bioinfo_id", "12345678_pass").Build()
+import PyGithub
+import base64
+
+g_inst = PyGithub.BlockingBuilder().Login("jayu108", "1234678").Build()
 user = g_inst.get_authenticated_user()
 
 print user.login
 print user.name
 print user.email
 print user.id
-
+print 
 
 rr = user.get_repos()
 
-	
+        
 for repo in rr:
-	print repo.name
-	print repo.full_name
-	print repo.description
-	repo.created_at
-	repo.updated_at
-	try:
-		cnts = repo.get_contents("")
-		# file document
-		# http://jacquev6.github.io/PyGithub/v2/reference/classes/File.html#PyGithub.Blocking.File.File		
-		for c in cnts:
-			print c.name, c.size, c.type, c.url
-			print ' 1------------ '
-			if c.type == u'file' :print c.content
-			print ' 2------------ '
-			print c.html_url			
-	except :
-		print '-- no contents --'
-	# x.creator
-	print
-	
-	
+        print repo.name
+        print repo.full_name
+        print repo.description
+        print ' repo.created_at ==> ', repo.created_at
+        print ' repo.updated_at ==> ',repo.updated_at
+        print ' repo.downloads_url  = ', repo.downloads_url
+        print ' repo.html_url ==> ',  repo.html_url
+        print ' repo.owner ==> ', repo.owner
+        print ' repo.pulls_url ==> ', repo.pulls_url
+        print ' repo.pushed_at ==> ', repo.pushed_at
+        print ' repo.size ==> ', repo.size
+        print ' repo.ssh_url ==> ', repo.ssh_url
+        print ' repo.statuses_url ==> ', repo.statuses_url
+
+        print ' repo.get_keys ==> ', repo.get_keys()
+        
+        print '         ===============         '
+##        try:
+##                cnts = repo.get_contents("")
+##                # file document
+##                # http://jacquev6.github.io/PyGithub/v2/reference/classes/File.html#PyGithub.Blocking.File.File         
+##                for c in cnts:
+##                        print c.name, c.size, c.type, c.html_url
+##                        print ' 1------------ '
+##                        if c.type == u'file' :
+##                                # print c.content   # base64 encoding  --  https://developer.github.com/v3/repos/contents/#contents
+##                                print base64.b64decode(c.content)
+##                        print ' 2------------ '
+##                        print c.html_url                        
+##        except :
+##                print '-- no contents --'
+##        # x.creator
+        print
+
+
+
+# repo.delete()    # delete repository !!!!
+
+
+# user.create_repo('test_bio11', 'test bio 11')   # create new repository !!
+
+
 
